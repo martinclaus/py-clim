2dd22b4f713b03c572752f4cee43da6aa44d526b
martinclaus <mclaus@geomar.de>
2014-03-19 08:46:59 +0100

    Stated work on lg_create_legend

--- pyNGL_contributed/ngl_contrib.py
+++ pyNGL_contributed/ngl_contrib.py
@@ -303,6 +303,103 @@ def tm_deg_labels(plot, ax="YL", fmt="%3.1f"):
     tm_lat_lon_labels(plot, ax, direction="any", fmt=fmt)
 
 
+def lg_create_legend(wks, vpXY=(.1, .9), vpWH=(.8, .8), shape=(2, 2),
+                     labels=["a", "b", "c", "d"], rlist=None):
+    refax = min(vpWH)
+    # set default values
+    if not rlist:
+        rlist = {}
+    ngl._set_legend_res(rlist, rlist)
+    margin = []
+    defaults = {"lgBoxBackground": -1,
+                "lgBoxLineColor": 0,
+                "lgBoxLineDashPattern": 0,
+                "lgBoxLineDashSegLenF": 0.15,
+                "lgBoxLineThicknessF": 1.0,
+                "lgBoxLinesOn": False,
+                "lgBoxMajorExtentF": .5,
+                "lgDashIndexes": [0 for l in labels],
+                "lgItemCount": len(labels),
+                "lgItemOrder": range(len(labels))[::-1],
+                "lgItemTypes": ["Lines" for l in labels],
+                "lgLabelAlignment": "ItemCenters",
+                "lgLabelAngleF": 0.0,
+                "lgLabelConstantSpacingF": 0.0,
+                "lgLabelDirection": "Across",
+                "lgLabelFont": "pwritx",
+                "lgLabelFontAspectF": 1.0,
+                "lgLabelFontColor": 0,
+                "lgLabelFontHeightF", 0.02,
+                "lgLabelFontQuality": "High",
+                "lgLabelFontThicknessF": 1.0,
+                "lgLabelFuncCode": ":",
+                "lgLabelJust": "CentreCentre",
+                "lgLabelOffsetF": 0.02,
+                "lgLabelPosition": "Right",
+                "lgLabelStride": 1,
+                "lgLabelsOn": True,
+                "lgLegendOn": True,
+                "lgLineColors": range(len(labels)) + 2,
+                "lgLineDashSegLenF": 0.15,
+                "lgLineLabelConstantSpacingF": 0.0,
+                "lgLineLabelFont": "pwritx",
+                "lgLineLabelFontAspectF": 1.0,
+                "":,}
+    for key in ("Top", "Bottom", "Left",  "Right"):
+        _set_default(rlist, "lg{}MarginF".format(key), .05)
+        margin[key] = rlist["lg{}MarginF".format(key)] * refax
+    if "lgMonoDashIndex" in rlist and rlist["lgMonoDashIndex"]:
+        _set_default(rlsit, "lgDashIndex", 0)
+    if "lgMonoItemType" in rlist and rlist["lgMonoDashIndex"]:
+        _set_default(rlist, "lgItemType", "Lines")
+    if "lgMonoLineColor" in rlist and rlist["lgMonoLineColor"]:
+        _set_default(rlist, "lgLineColor", 0)
+    lgBoxLineDashPattern 0
+
+
+    # calculate positions
+    itemPanelXY = (vpXY[0] + margin["Left"], vpXY[1] - margin["Top"])
+    itemPanelWH = (vpWH[0] - margin["Left"] - margin["Right"],
+                   vpWH[1] - margin["Top"] - margin["Bottom"])
+
+#    leg_labels = ["GB2012", "Full", "Equator", "West", "Centre", "East"]
+
+    leg_lineLength = .075
+    leg_xMarker = .2 + np.linspace(0., .2, 2)
+    leg_yMarker = .1 + np.linspace(.08, 0., 3)
+
+    marker_ind = [None] + res.xyMarkers
+    marker_res = ngl.Resources()
+    marker_res.gsMarkerColor = "black"
+    marker_res.gsMarkerThicknessF = 2.
+    marker_res.gsMarkerSizeF = .01
+
+    line_dash_ind = [0] + res.xyDashPatterns
+    line_res = ngl.Resources()
+    line_res.gsLineColor = "black"
+    line_res.gsLineThicknessF = 2.
+
+    lab_res = ngl.Resources()
+    lab_res.txFontHeightF = large_font
+    lab_res.txJust = "CenterLeft"
+
+
+    ind = 0
+    for mx in leg_xMarker:
+        for my in leg_yMarker:
+            line_res.gsLineDashPattern = line_dash_ind[ind]
+            ngl.polyline_ndc(wks,
+                             [mx - leg_lineLength / 2., mx + leg_lineLength / 2.],
+                             [my, my], line_res)
+            if not marker_ind[ind] == None:
+                marker_res.gsMarkerIndex = marker_ind[ind]
+                ngl.polymarker_ndc(wks, mx, my, marker_res)
+            ngl.text_ndc(wks, leg_labels[ind],
+                         mx + leg_lineLength / 2. + .01, my, lab_res)
+            ind += 1
+
+
+
 def lb_create_labelbar(wks, vpXY, vpWH, nboxes=11, levels=(-1., 1.),
                        fmt_str="{}", rlist=None):
     '''Creates a label bar.
@@ -664,3 +761,8 @@ def _dict2Resource(dic):
     res = ngl.Resources()
     res.__dict__.update(dic)
     return res
+
+
+def _set_default(rdic, attrib, val):
+    if not attrib in rdic:
+        rdic[attrib] = val
